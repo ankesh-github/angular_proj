8c6bb5d6698758833156431cd982cd424f823980
Ankesh Tiwari <ankeshtiwari46@gmail.com>
2025-10-25 12:16:23 +0530

    example for customcomponent in angular

---
+++ src/app/profile/profile.ts
@@ -0,0 +1,27 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'app-profile',
+    templateUrl: './profile.html',
+    styleUrl: './profile.css'
+
+})
+
+export class Profile {
+
+}
+
+
+// import {Component} from "@angular/core";
+
+// @Component({
+//     selector: 'app-profile',
+//     template: '<h1>Profile Component</h1>',
+//     styles: 'h1{background-color : pink}'
+// })
+
+// export class Profile {
+
+// }
+
+
