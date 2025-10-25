3414eee3fe0728f80d0183bc2eaa4f0340daf952
Ankesh Tiwari <ankeshtiwari46@gmail.com>
2025-10-25 22:47:25 +0530

    example for css styles and rules

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -3,25 +3,13 @@ import {Component} from "@angular/core";
 @Component({
     selector: 'app-profile',
     templateUrl: './profile.html',
-    styleUrl: './profile.css'
+    //styleUrl: './profile.css'
+    styles: ["h1{ background-color: red}"]
 
 })
 
 export class Profile {
 
 }
-
-
-// import {Component} from "@angular/core";
-
-// @Component({
-//     selector: 'app-profile',
-//     template: '<h1>Profile Component</h1>',
-//     styles: 'h1{background-color : pink}'
-// })
-
-// export class Profile {
-
-// }
 
 
