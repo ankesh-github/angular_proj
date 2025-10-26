dbebf7971601523215545b6e65e32375257d63c5
Ankesh Tiwari <ankeshtiwari46@gmail.com>
2025-10-26 23:08:23 +0530

    example for template driven form validation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
+import { NgIf } from '@angular/common';
 import { Component} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
