2a7a8ea874c3819042eae641948cdac3ae3decef
Ankesh Tiwari <ankeshtiwari46@gmail.com>
2025-10-26 23:32:06 +0530

    example to pass data from parent to child component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,19 @@
-import { NgIf } from '@angular/common';
+import {} from '@angular/common';
 import { Component} from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import {} from '@angular/forms';
+import { User } from "./user/user";
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, NgIf],
+  imports: [User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  userName="Bruce";
 
-  userData:any;
-
-  addUser(val : NgForm){
-    console.log(val);
-    this.userData=val;
+  changeUser(val : string){
+    this.userName=val;
   }
 
 }
