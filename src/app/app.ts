270bdc677a1e27bd8b5c3c6affc5a5da239a78dc
Ankesh Tiwari <ankeshtiwari46@gmail.com>
2025-10-26 11:38:57 +0530

    example for ngFor directive

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,21 +3,22 @@ import { RouterOutlet } from '@angular/router';
 import { Login } from "./login/login";
 import { Signup } from "./signup/signup";
 import { Profile } from "./profile/profile";
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  color= 'red';
-  fontSizeNormal="50px";
-  fontSizeBig="60px";
-  zoom=true;
-  size=60
+  students=["Ramesh","Mahesh","Suresh"];
 
-  zoomToggle(){
-    this.zoom =!this.zoom;
-  }
+  studentData=[
+    {name:"Ramesh",age:"32",email:"ramesh@gmail.com"},
+    {name:"Mahesh",age:"34",email:"mahesh@gmail.com"},
+    {name:"Suresh",age:"30",email:"suresh@gmail.com"},
+    {name:"rajesh",age:"38",email:"rajesh@gmail.com"},
+  ]
+  
 }
