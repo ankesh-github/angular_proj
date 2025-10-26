51a3838647b26cb7ff76269e0042977958ab815d
Ankesh Tiwari <ankeshtiwari46@gmail.com>
2025-10-26 19:36:22 +0530

    example for basic form data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,12 +7,14 @@ import { PageNotFound } from './page-not-found/page-not-found';
 import { User } from './user/user';
 import { Profile } from './profile/profile';
 
-export const routes: Routes = [
-    {path:'login',component:Login},
-    {path:'about',component:About},
-    {path:'contact',component:Contact},
-    {path:'',component:Home},
-    {path:'user/:id/:name',component:User},
-    {path:'profile',component:Profile},
-    {path:'**',component:PageNotFound},
-];
+// export const routes: Routes = [
+//     {path:'login',component:Login},
+//     {path:'about',component:About},
+//     {path:'contact',component:Contact},
+//     {path:'',component:Home},
+//     {path:'user/:id/:name',component:User},
+//     {path:'profile',component:Profile},
+//     {path:'**',component:PageNotFound},
+// ];
+
+export const routes: Routes = []
