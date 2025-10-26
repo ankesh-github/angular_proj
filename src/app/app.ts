22011f843504376ee499f0dae3c894d26d38fbdc
Ankesh Tiwari <ankeshtiwari46@gmail.com>
2025-10-26 22:43:58 +0530

    example for template driven form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,46 +1,19 @@
-import { Component, computed, effect, signal, Signal, WritableSignal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import { Component} from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [ReactiveFormsModule, NgIf],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  profileForm =new FormGroup({
-    name:new FormControl('',[Validators.required]),
-    email:new FormControl('', [Validators.required, Validators.min(5)]),
-    password:new FormControl('', [Validators.required, Validators.maxLength(10)]),
 
-  });
+  userData:any;
 
-  submitData(){
-    console.log(this.profileForm.value)
+  addUser(val : NgForm){
+    console.log(val);
+    this.userData=val;
   }
-
-  get name(){
-    return this.profileForm.get('name');
-  }
-
-  get email(){
-    return this.profileForm.get('email');
-  }
-
-  get password(){
-    return this.profileForm.get('password');
-  }
-
-
-  setValue(){
-    this.profileForm.setValue({
-      name:'peter',
-      email:'peter@gmail.com',
-      password:'pass123'
-    })
-  }
-
 
 }
