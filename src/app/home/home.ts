7b325b960ffa4e606979773e88310ff753b4f67a
Ankesh Tiwari <ankeshtiwari46@gmail.com>
2025-10-26 16:29:09 +0530

    example for dynamic routing

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  users = [
+    { id: 1, name: "Ramesh", email: "ramesh@gmail.com" },
+    { id: 1, name: "Mahesh", email: "mahesh@gmail.com" },
+    { id: 1, name: "Narsh", email: "narsh@gmail.com" },
+    { id: 1, name: "Suresh", email: "suresh@gmail.com" }
+  ]
 
 }
