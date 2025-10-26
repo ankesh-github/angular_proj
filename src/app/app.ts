da8cd149fc4fcd3446ad05a65e244d75db9700d0
Ankesh Tiwari <ankeshtiwari46@gmail.com>
2025-10-26 15:23:18 +0530

    basic setup for routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,15 @@
 import { Component, computed, effect, signal, Signal, WritableSignal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Login } from "./login/login";
 import { Signup } from "./signup/signup";
 import { Profile } from "./profile/profile";
-import { NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [NgSwitch , NgSwitchCase, NgSwitchDefault],
+  imports: [RouterOutlet, RouterLink ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  color="black";
-
-  changeColor(color:string){
-    this.color = color;
-
-  }
  
 }
