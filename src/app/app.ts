cdd0ce6b1e5855b7d6c93cdec8872d33bcf1c1e4
Ankesh Tiwari <ankeshtiwari46@gmail.com>
2025-10-27 11:02:57 +0530

    life cycle demo

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,27 @@ import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
 import {} from '@angular/forms';
 import { CurrencyConverterPipe } from './pipe/currency-converter-pipe';
+import { User } from "./user/user";
 
 @Component({
   selector: 'app-root',
-  imports: [CurrencyConverterPipe],
+  imports: [User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
- amount=10;
+  constructor(){
+    console.log("constructor");
+  }
+
+  ngOnInIt(){
+    console.log("ngOnInIt");
+  }
+
+  ngOnDestroy(){
+    console.log("ngOnDestroy");
+  }
+
+
 
 }
