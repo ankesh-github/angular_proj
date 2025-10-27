6897717c352013bc17596b05e04591dcfe915ec4
Ankesh Tiwari <ankeshtiwari46@gmail.com>
2025-10-27 10:32:20 +0530

    example for pipes in angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,16 @@
-import {} from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
 import {} from '@angular/forms';
-import { User } from "./user/user";
 
 @Component({
   selector: 'app-root',
-  imports: [User],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  users:string[] | undefined
-  handleUsers(users:string[]){
-    console.log(users);
-    this.users=users;
-  }
+  title = "Pipes Demo";
+  today = new Date();
+  amount=10;
 
 }
