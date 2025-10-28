228af9a39d500e186177fe19d324a9c4c47a1bf0
Ankesh Tiwari <ankeshtiwari46@gmail.com>
2025-10-28 15:44:09 +0530

    exaple for api call in angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { } from '@angular/forms';
+import { Users } from './services/users';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,16 @@ import { } from '@angular/forms';
   styleUrl: './app.css'
 })
 export class App {
+  users:any;
+  constructor(private userService : Users){};
+
+  ngOnInit(){
+    this.userService.getUsers().subscribe(
+      (data:any)=>{
+        console.log(data);
+        this.users=data;
+      }
+    );
+  }
 
 }
