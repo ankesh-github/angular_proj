103435792e9d0b6830a2d5dd2edc850b297226ac
Ankesh Tiwari <ankeshtiwari46@gmail.com>
2025-10-28 17:49:24 +0530

    demo for put and delete method

--- src/app/services/users.ts
+++ src/app/services/users.ts
@@ -7,16 +7,31 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class Users {
+  url:string="http://localhost:3000/users";
+
   constructor(private http : HttpClient){}
 
   getUsers() : Observable<User[]>{
-    const url="http://localhost:3000/users";
-    return this.http.get<User[]>(url);
+    return this.http.get<User[]>(this.url);
   }
 
   saveUsers(user:User) : Observable<User>{
-    const url="http://localhost:3000/users";
-    return this.http.post<User>(url, user);
+    return this.http.post<User>(this.url, user);
   }
+
+  deleteUser(id:string):Observable<User>{
+    return this.http.delete<User>(this.url+"/"+id);
+  }
+
+  getSelectedUser(id:string):Observable<User>{
+    return this.http.get<User>(this.url+"/"+id);
+  }
+
+  updateUser(user:User): Observable<User>{
+    return this.http.put<User>(this.url+"/"+user.id, user);
+  }
+  
+
+
   
 }
