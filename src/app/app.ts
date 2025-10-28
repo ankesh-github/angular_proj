3f9d7a138789917235cf114817558eee803ea743
Ankesh Tiwari <ankeshtiwari46@gmail.com>
2025-10-28 15:57:24 +0530

    example for interface in angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { } from '@angular/forms';
 import { Users } from './services/users';
+import { User } from './interfaces/User';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,12 @@ import { Users } from './services/users';
   styleUrl: './app.css'
 })
 export class App {
-  users:any;
+  users:User[]=[];
   constructor(private userService : Users){};
 
   ngOnInit(){
     this.userService.getUsers().subscribe(
-      (data:any)=>{
+      (data:User[])=>{
         console.log(data);
         this.users=data;
       }
